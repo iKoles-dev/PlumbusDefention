07ba76580af28c3f240acf6f67296fe2048b71b8
iKoles-dev <59211303+iKoles-dev@users.noreply.github.com>
2020-02-21 06:43:17 +0300

    [FIX] Small changes
    Correct missing scripts on buying towers
    Connect bying price with scriptable objects

---
+++ Assets/Scripts/Towers/BuyingTower.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BuyingTower : MonoBehaviour
+{
+    [SerializeField] private Tower _tower;
+    [SerializeField] private TextMeshProUGUI _price;
+
+    private void Start()
+    {
+        _price.text = _tower.BuyingCost.ToString();
+    }
+}
